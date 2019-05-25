229c6c8bce13ebddefb5a6bc91627db0b3cecba1
ekwemefranklinokenna <ekwemefranklinokenna@gmail.com>
2019-05-25 10:51:32 +0100

    the Login page

---
+++ Login_page.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Rgistration::Portal</title>
+<link rel="stylesheet" href="stylesheet.css"/>
+</head>
+   
+<body>
+     <div id="menu"><h2>WEBAPP:NAME</h2></div>
+      <div id="box1">
+            <div id="box2"><h1>Login</h1><hr />
+              <form>
+                  <label>User-Name:</label><br />
+                  <input type="text" id="inputid" required/><br /><br />
+                  <label>Pasword:</label><br />
+                  <input  type="password" id="inputid" required/><br /><a href="#"><h4>Forgotten Password </h4></a> <br />
+                  <input type="submit" id="submitid" />
+              </form><br />
+              <a href="#" id="signupLink"><h3>Sign-Up</h3></a>
+          </div>
+    </div>
+</body>
+</html>
